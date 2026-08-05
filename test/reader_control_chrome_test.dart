9a31f2d7b15179833e674d798ad6b1515a3c906b
WuLiuQi1 <w885280077@gmail.com>
2026-08-05 22:12:47 +0800

    v0.4.17

--- test/reader_control_chrome_test.dart
+++ test/reader_control_chrome_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:xxread/core/reader/reader_leaf_status.dart';
